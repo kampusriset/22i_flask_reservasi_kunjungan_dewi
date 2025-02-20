7f5d0d31749571251ffed63fb0e2f1d64312eb00
shenashanny <shenashanny13@gmail.com>
2025-02-20 09:19:52 +0000

    Create shanny.py

---
+++ team/shanny.py
@@ -0,0 +1,14 @@
+# Perkenalan shanny
+def perkenalan():
+    nama = "Shanny"
+    nim = "481"
+    peran = "Membantu viona dalam membuat coding"
+    deskripsi = (
+        f"Halo, saya {nama} (NIM: {nim}).\n"
+        "Saya bertugas membantu viona dalam membuat codingan.\n"
+        "Membantu dalam konsep tampilan serta mengedit video"
+    )
+    print(deskripsi)
+
+if __name__ == "_main_":
+    perkenalan()
