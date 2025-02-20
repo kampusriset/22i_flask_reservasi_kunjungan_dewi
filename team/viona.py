489e7a36a2f96b9ab4a9b36e767ea5f7d711236a
viona27 <vionalenyanjani27@gmail.com>
2025-02-20 11:30:17 +0700

    create viona

---
+++ team/viona.py
@@ -0,0 +1,14 @@
+# Perkenalan Viona
+def perkenalan():
+    nama = "Viona"
+    nim = "473"
+    peran = "Pembuat codingnya"
+    deskripsi = (
+        f"Halo, saya {nama} (NIM: {nim}).\n"
+        "Saya bertugas sebagai pembuat coding dalam proyek ini.\n"
+        "Saya memastikan bahwa setiap fitur dalam aplikasi dikembangkan dengan baik dan berjalan sesuai harapan."
+    )
+    print(deskripsi)
+
+if __name__ == "_main_":
+    perkenalan()
