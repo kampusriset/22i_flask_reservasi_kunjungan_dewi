a77911dd1ecd6378d3dc92faa054a8e08cf7dc8f
ulfah1 <ulfahnuruzlifah04@gmail.com>
2025-02-20 18:46:49 +0700

    Update ulfah.py

--- team/ulfah.py
+++ team/ulfah.py
@@ -1,10 +1,14 @@
-def perkenalan(nama, nim, peran):
-    print("Halo! Perkenalkan, nama saya", nama)
-    print("NIM : ", nim)
-    print("Peran saya yaitu : ", peran)
+# Perkenalan Ulfah
+def perkenalan():
+    nama = "Ulfah"
+    nim = "488"
+    peran = "Membuat coding untuk tampilan"
+    deskripsi = (
+        f"Halo, saya {nama} (NIM: {nim}).\n"
+        "Saya berperan sebagai pembuat coding untuk tampilan.\n"
+        "Saya memastikan untuk setiap tampilan fitur dalam aplikasi dikembangkan dengan baik."
+    )
+    print(deskripsi)
 
-if _name_ == "_main_":
-    nama = "Ulfah Nur Uzlifah"
-    nim = "2213010488"
-    peran = "Mengerjakan coding dan memperbaiki tampilan"
-    perkenalan(nama, nim, peran)
+if __name__ == "_main_":
+    perkenalan()
